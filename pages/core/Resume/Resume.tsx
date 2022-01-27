271e4ec54118d227ef9e66d2f59eef9a599c33ea
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-01-26 21:09:13 -0600

    move the viewer out of the pages

--- pages/core/Resume/Resume.tsx
+++ pages/core/Resume/Resume.tsx
@@ -4,7 +4,9 @@ import { Flex, Spinner } from "@chakra-ui/react"
 import dynamic from "next/dynamic"
 
 export const Resume: FC = () => {
-  const Viewer = dynamic(() => import("./Viewer"), { ssr: false })
+  const Viewer = dynamic(() => import("../../../components/Viewer"), {
+    ssr: false,
+  })
 
   return (
     <Flex w="full" h="full">
