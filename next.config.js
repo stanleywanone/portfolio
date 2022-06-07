c6107f8792fcee1d5b84609e67643f04d24af8b4
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-06-07 08:48:57 -0500

    just use the subfolder to have a sub-path

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "https://vercel.com/stanleywanone/portfolio/swp",
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/core/Biography",
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/core/Biography",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, useEffect } from "react"
-
-export const Home: FC = () => {
-  return <div>this is the index page</div>
-}
-
-export default Home
