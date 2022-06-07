bf412f80e38d8b0cf032d83c7fa9305ff698f21d
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-06-06 20:50:09 -0500

    add the assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: " https://portfolio-p3vvlny9z-stanleywanone.vercel.app/swp",
   basePath: "/swp",
   async redirects() {
     return [
