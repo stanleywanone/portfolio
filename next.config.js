0d971e4700f665d1cf9bb4b0a6b4ffd367d539a7
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-06-06 20:29:49 -0500

    add basePath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/swp",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/swp/core/Biography",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
 
-module.exports = {
-  distDir: "build",
-}
+// module.exports = {
+//   distDir: "build",
+// }
