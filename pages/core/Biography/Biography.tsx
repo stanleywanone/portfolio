f6df931d869581a478b74c46c154a9171ba20aa3
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-06-28 15:20:00 -0500

    update the Biography

--- pages/core/Biography/Biography.tsx
+++ pages/core/Biography/Biography.tsx
@@ -4,6 +4,7 @@ import {
   Flex,
   Text,
   Spacer,
+  HStack,
   List,
   ListItem,
   ListIcon,
@@ -13,36 +14,39 @@ import {
 export const Biography: FC = () => {
   return (
     <Flex w="full" h="full" flexDirection="column">
-      <Text
-        fontFamily="monospace"
-        fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
-        fontWeight="bold"
-        mb="2%"
-      >
-        Biography
-      </Text>
-      <Text fontSize="xl">
-        Hi, I’m Stanley Wan, currently working as a UI Engineer (Member of
-        Technical Staff) at Hypergiant in the SaaS Platform team. I have a
-        Master’s degree in CS from CSU, Fresno State and a Master’s in MSE from
-        the Feng Chia University in Taiwan.
-        <Spacer mb="2%" />
-        Previously, I worked at Asiayo, Taiwan as a Full Stack Software
-        Engineer, where I worked on designing front-end features across multiple
-        platforms using modern industry-adopted languages and framework and
-        Optimizing API calls using modular application design and light weight
-        dependencies. Aside from work, I am also an open-source enthusiast and
-        have created projects that were listed in GitHub trending.{" "}
-        <Spacer mb="2%" />
-        <Spacer />
-        My interests lie in delivering the concept of ideas on the front-end
-        features related to mutilple areas of software engineerings.
-      </Text>
-      <Flex w="full" mt={10}>
+      <Flex flexDirection="column">
+        <Text
+          fontFamily="monospace"
+          fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}
+          fontWeight="bold"
+          mb="2%"
+        >
+          Biography
+        </Text>
+        <Text fontSize="xl">
+          Hi, I’m Stanley Wan, currently working as a UI Engineer at Apple in
+          the Retail App team. I have a Master’s degree in CS from CSU, Fresno
+          State and a Master’s in MSE from the Feng Chia University in Taiwan.
+          <Spacer mb="2%" />
+          In Apple, I’m responsible for developing a SaaS platform which help
+          the data scientises and maching learning engineers monior and anylsis
+          the data more straightforward. On the other hand, I worked on
+          designing front-end features across multiple platforms using modern
+          industry-adopted languages and framework and Optimizing API calls
+          using modular application design and light weight dependencies. Aside
+          from work, I am also an open-source enthusiast and have created
+          projects that were listed in GitHub trending. <Spacer mb="2%" />
+          <Spacer />
+          My interests lie in delivering the concept of ideas on the front-end
+          features related to mutilple areas of software engineerings.
+        </Text>
+      </Flex>
+      <HStack mt={10} flexDirection="row">
         <Flex flexDirection="column" w="50%">
           <Text fontSize="2xl" fontWeight="bold" fontFamily="monospace">
             Interests
           </Text>
+
           <UnorderedList ml="10%">
             <ListItem>Front-End Engineer</ListItem>
             <ListItem>UI Engineer</ListItem>
@@ -67,7 +71,7 @@ export const Biography: FC = () => {
             <Text fontSize="xs">Feng Chia University, Taiwan</Text>
           </List>
         </Flex>
-      </Flex>
+      </HStack>
     </Flex>
   )
 }
